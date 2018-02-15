757424e257b80fbfbf44ab20aff2957ac7516a25
Venkata Naga Kartik Pidatala <pidatala@usc.edu>
2018-02-14 20:24:20 -0800

    Removing unecessary comments

--- RELIABLE FTP/client_final_1.c
+++ RELIABLE FTP/client_final_1.c
@@ -56,7 +56,6 @@ int main (int argc, char *argv[])
     socklen_t fromlen;
     struct iphdr *iph = (struct iphdr *) datagram;
     struct sockaddr_in sin;
-    //zero out the packet buffer
     struct timeval time_out;
     time_out.tv_sec=0;
     time_out.tv_usec=350000;
@@ -73,9 +72,6 @@ int main (int argc, char *argv[])
     memset(datagram,0,5016);
   if(recvfrom(s,datagram,65535,0,&addr,&fromlen)<0)
        perror("Receive failed");
-    else
-      {//printf("\n Received Success in file size ");
-}
 
     memcpy(&recvd_seq,datagram+sizeof(struct iphdr),4);
     recvd_seq=ntohl(recvd_seq);
@@ -83,7 +79,6 @@ int main (int argc, char *argv[])
     {
       memcpy(&filesize,datagram+sizeof(struct iphdr)+4,4);
       filesize=ntohl(filesize);
-//      printf("\n Filesize got from server : %d",filesize);
     }
     else
     {
@@ -126,9 +121,7 @@ iph->version = 4;
     {
         perror("sendto failed");
     }
-    else
-   {} 
-    //    printf ("Packet Sending ACK. Length & Sequence: %d \n" , iph->tot_len);
+  
 
     int i=filesize/1475;
     int num_of_packets = i;
@@ -147,7 +140,7 @@ iph->version = 4;
 
     fd=fileno(newfile);
     ftruncate(fd,filesize);
-    //printf("\n Assigning temp data");
+    
     temp_data=mmap(NULL,(filesize),PROT_READ | PROT_WRITE,MAP_SHARED,fd,0);
     if(temp_data==MAP_FAILED)
     {
@@ -163,7 +156,7 @@ iph->version = 4;
     /*** Receiving data from the sender ***/
     while (i>=0)
     {
-      //printf("\n Recvd : %d",i);
+      
         if(recvfrom(s,datagram,65535,0,&addr,&fromlen)<0)
         {
             //perror("Error reading 1");
@@ -173,34 +166,22 @@ iph->version = 4;
     memcpy(&recvd_seq,datagram+sizeof(struct iphdr),4);
     recvd_seq=ntohl(recvd_seq);
     if (recvd_seq==0){
-        /*** Sending ACK again for file size ***/
-     /*   if (sendto (s, datagram, iph->tot_len ,  0, (struct sockaddr *) &sin, sizeof (sin)) < 0)
-        {
-            perror("sendto failed");
-        }
-        else
-        {
-            printf ("Packet Send. Length & Sequence: %d \n" , iph->tot_len);
-        }*/
+       
         continue;
    }
 
-    //if (i!=(filesize/1475)) {
+   
         nack[recvd_seq]= '1';
-    //}
-   // printf("\n Entered While : %d, i = %d",recvd_seq,i);
+   
+  
 
     //Data part
     data = datagram + sizeof(struct iphdr) + 4;//+ sizeof(struct tcphdr)
     if(recvd_seq >filesize)
        recvd_seq=1;
     memcpy((temp_data + ((recvd_seq-1) * 1475)),data, recvd_seq == (num_of_packets+1)? last_packet_size : 1475);
-   // printf("\n Done memcopy for seq number : %d ",recvd_seq);
-    //printf("\n i : %d",i);
-    //temp_data=temp_data+strlen(data);
     i--;
     }
-  //printf("\n Packets received and checking for holes filesize received \n");
 /* Check to be removed */
 
     int k,flag=1,m;
@@ -256,31 +237,21 @@ iph->version = 4;
 
        if(i_c==367 || ((k==num_of_packets+1) && flag))
        {
-        //    printf("\n NACKed packets still available");
             *data='\0';
             tot_len=sizeof(struct iphdr)+1+(i_c*4)+2;
             i_c=htons(i_c);
             memcpy((datagram+sizeof(struct iphdr)+1),&i_c,2);
             
             iph->tot_len=htons(tot_len);
-            //printf("\n Nack pack number : %d data_len=%d ,i_c in htons = %d\n",m, tot_len,i_c);
             iph->check = csum ((unsigned short *) datagram, tot_len); // Print the seq number and see for cross verification
                     int i = 0;
-                /*    for(i = 0;i < 100; i+=10) {
-                        printf("%0.2x %0.2x %0.2x %0.2x %0.2x %0.2x %0.2x %0.2x %0.2x %0.2x \n",
-                                datagram[i], datagram[i+1], datagram[i+2], datagram[i+3],  datagram[i+4],
-                                datagram[i+5], datagram[i+6], datagram[i+7], datagram[i+8],  datagram[i+9]);
-                    }*/
-
             if (sendto (s, datagram, tot_len ,  0, (struct sockaddr *) &sin, sizeof (sin)) < 0)
             {
-           // perror("sendto failed");
+            perror("sendto failed");
             }
            else
            {
             m++;
-           // printf ("Packet Send. Length & Sequence: %d \n" , m);
-            //printf("\n Nack pack number : %d data_len=%d",m, tot_len);
             data = datagram + sizeof(struct iphdr);
             memset(data,0,1480);
             *data='1';
@@ -289,30 +260,22 @@ iph->version = 4;
            }
         }
      }
-    //printf("\n Outside for loop :");
-    //printf("\n Waiting for lost packets ");
     while(flag)  /* NACK is a integer array pointer*/
    {
-//        memset(datagram, 0, 5016);
         if(recvfrom(s,datagram,65535,0,&addr,&fromlen)<0){ //  Receiving the lost packets
-        //perror("Receive failed 1");
         break;
         }
         else
         {
-          //  printf("\n Received Datagram");
             memcpy(&lost_seq,datagram+sizeof(struct iphdr),4);
             lost_seq=ntohl(lost_seq);
             nack[lost_seq]= '1';
             int pack_size=(lost_seq==num_of_packets+1)? last_packet_size : 1475;
-           // printf("\n Received  the lost packet %d, size %d",lost_seq,pack_size);
             memcpy((temp_data+((lost_seq-1)*(1475))),datagram+sizeof(struct iphdr)+4,lost_seq == (num_of_packets+1)? last_packet_size :
  1475);
-          //  printf("\n Memcopy successfll");
         }
              /* Copying the data into the buffer.*/
      }
-  // printf("\n Done receiving lost packets");
    }while(flag==1);
 
 free(nack);
@@ -331,20 +294,14 @@ k=0;
     k++;
     if (sendto (s, datagram, iph->tot_len ,  0, (struct sockaddr *) &sin, sizeof (sin)) < 0)
         {
-           // perror("sendto failed");
-        }
-    else
-        {
-          //  printf ("Packet Send. Length & Sequence: %d \n" , iph->tot_len);
-        }
+            perror("sendto failed");
+          }
     }
 
 /*** Write into file and close ***/
 
     struct timeval time1;
     gettimeofday(&time1,NULL);
-    //printf("\n end time %d : %d\n",(int)time1.tv_sec,(int)time1.tv_usec);
-    //fwrite(recvdata,1,filesize,newfile);
     fclose(newfile) ;
     close(s);
     munmap(recvdata,filesize);
